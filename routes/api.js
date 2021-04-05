724f8b0b3e69a8b7eb84b68ac27bff9f69125457
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-04-05 20:31:19 +0200

    put request solved

--- routes/api.js
+++ routes/api.js
@@ -15,7 +15,29 @@ module.exports = function (app) {
     
     .post(function (req, res){
       let project = req.params.project;
-      
+      const {
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text
+      } = req.body;
+
+      if(!issue_title || !issue_text || !created_by) {
+        res.json({ error: 'required field(s) missing'});
+        return;
+      }
+
+      const newIssue = new IssueModel({
+        issue_title: issue_title || '',
+        issue_text: issue_text || '',
+        created_on: new Date(),
+        updated_on: new Date(),
+        created_by: created_by || '',
+        assigned_to: assigned_to || '',
+        open: true,
+        status_text: status_text || ''
+      });
     })
     
     .put(function (req, res){
